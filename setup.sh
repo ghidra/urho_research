c4f9a7942d8819a9a9e8c932bb9b66f4934fa3ad
ghidra <ghidra@gmail.com>
2014-12-13 22:14:52 -0500

    shell script update

--- setup.sh
+++ setup.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
 URHOPATH=$1
-URHODATA=$URHOPATH"/Bin/Data/"
+URHOBINPATH=$URHOPATH"/Bin/"
+URHODATAPATH=$URHOPATH"/Bin/Data/"
 
 # Absolute path this script is in, thus /home/user/bin
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-echo $SCRIPTPATH
-echo $URHODATA
+echo "script path:"$SCRIPTPATH
+echo "urho path:"$URHODATAPATH
 
 
 for Dir in $(find $SCRIPTPATH* -mindepth 1 -maxdepth 1 -not -path '*/\.*' -type d );
 do
     FOLDER=$(basename $Dir);
+
+    #first we need to check that the link we want to make already exist or not
     if [ "$FOLDER" == "Scripts" ]; then
-	     echo "Want to move the script folder"
+      #http://stackoverflow.com/questions/59838/check-if-a-directory-exists-in-a-shell-script
+      NEWFOLDER=$URHODATAPATH$FOLDER"/research"
+      if [ ! -d $NEWFOLDER ];then
+        #make the link
+        ln -s $SCRIPTPATH"/"$FOLDER $NEWFOLDER
+        #echo "copy from:"$SCRIPTPATH"/"$FOLDER
+        echo "Create the Script folder:"$NEWFOLDER
+      fi
     else
-	     echo "You want to move the $FOLDER folder"
+      NEWFOLDER=$URHOBINPATH"Extra/"$FOLDER
+      if [ ! -d $NEWFOLDER ];then
+        ln -s $SCRIPTPATH"/"$FOLDER $NEWFOLDER
+        echo "You want to move the $FOLDER folder:"$NEWFOLDER
+      fi
     fi
 
 done
