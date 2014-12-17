b1fce005e1be153f5f2d2d09fb3a87f8fdc4cb0a
ghidra <ghidra@gmail.com>
2014-12-16 20:38:34 -0500

    bash script functioning

--- setup.sh
+++ setup.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 
+make_alias(){
+  #$1 FOLDER $2 LINKEDFOLDER $3 NEWFOLDER
+  if [ ! -d $3 ];then
+    #link does not exist, we can make it
+    ln -s $2 $3
+    echo "Created link to" $1 "at" $3
+  else
+    echo $1 "Folder already exists as" $3
+  fi
+}
+
 URHOPATH=$1
-URHOBINPATH=$URHOPATH"/Bin/"
-URHODATAPATH=$URHOPATH"/Bin/Data/"
+#first make sure that the given folder is good
 
-# Absolute path this script is in, thus /home/user/bin
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+if [ -d $URHOPATH ];then
 
-echo "script path:"$SCRIPTPATH
-echo "urho path:"$URHODATAPATH
+  URHOBINPATH=$URHOPATH"/Bin/"
+  URHODATAPATH=$URHOPATH"/Bin/Data/"
+  URHOCOREDATAPATH=$URHOPATH"/Bin/CoreData/"
 
+  # Absolute path this script is in, thus /home/user/bin
+  SCRIPT=$(readlink -f "$0")
+  SCRIPTPATH=$(dirname "$SCRIPT")
 
-for Dir in $(find $SCRIPTPATH* -mindepth 1 -maxdepth 1 -not -path '*/\.*' -type d );
-do
-    FOLDER=$(basename $Dir);
+  echo "script path:"$SCRIPTPATH
+  echo "urho path:"$URHODATAPATH
 
-    #first we need to check that the link we want to make already exist or not
-    if [ "$FOLDER" == "Scripts" ]; then
-      #http://stackoverflow.com/questions/59838/check-if-a-directory-exists-in-a-shell-script
-      NEWFOLDER=$URHODATAPATH$FOLDER"/research"
-      if [ ! -d $NEWFOLDER ];then
-        #make the link
-        ln -s $SCRIPTPATH"/"$FOLDER $NEWFOLDER
-        #echo "copy from:"$SCRIPTPATH"/"$FOLDER
-        echo "Create the Script folder:"$NEWFOLDER
-      fi
-    else
-      NEWFOLDER=$URHOBINPATH"Extra/"$FOLDER
-      if [ ! -d $NEWFOLDER ];then
-        ln -s $SCRIPTPATH"/"$FOLDER $NEWFOLDER
-        echo "You want to move the $FOLDER folder:"$NEWFOLDER
-      fi
-    fi
+  for Dir in $(find $SCRIPTPATH* -mindepth 1 -maxdepth 1 -not -path '*/\.*' -type d );
+  do
+      FOLDER=$(basename $Dir);
+      case $FOLDER in
+        "Scripts")make_alias $FOLDER $SCRIPTPATH"/"$FOLDER $URHODATAPATH$FOLDER"/research" ;;
+        "RenderPaths") make_alias $FOLDER $SCRIPTPATH"/"$FOLDER $URHOCOREDATAPATH$FOLDER"/research" ;;
+        "Techniques") make_alias $FOLDER $SCRIPTPATH"/"$FOLDER $URHOCOREDATAPATH$FOLDER"/research" ;;
+        "Shaders") make_alias $FOLDER $SCRIPTPATH"/"$FOLDER $URHOCOREDATAPATH$FOLDER"/research" ;;
+        "Materials") make_alias $FOLDER $SCRIPTPATH"/"$FOLDER $URHODATAPATH$FOLDER"/research" ;;
+        *) echo "no commands for:" $FOLDER ;;
+      esac
 
-done
+  done
+
+else
+  echo "invalid path given:" $URHOPATH
+fi
