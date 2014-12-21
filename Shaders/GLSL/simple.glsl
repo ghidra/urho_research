a2006ff7bd68c14a07d9187d34907f7db0788d38
ghidra <ghidra@gmail.com>
2014-12-20 21:22:18 -0500

    mostly working single pixel

--- Shaders/GLSL/simple.glsl
+++ Shaders/GLSL/simple.glsl
@@ -18,11 +18,11 @@ float threshold(in float thr1, in float thr2 , in float val) {
 }
 
 // averaged pixel intensity from 3 color channels
-float avg_intensity(in vec4 pix) {
- return (pix.r + pix.g + pix.b)/3.0;
-}
+//float avg_intensity(in vec4 pix) {
+// return (pix.r + pix.g + pix.b)/3.0;
+//}
 float color_difference(in vec4 sc, in vec4 nc){
-  return abs(sc.r-nc.r)+abs(sc.g-nc.g)+abs(sc.b-nc.b)+(1.0-nc.a);
+  return abs(sc.r-nc.r)+abs(sc.g-nc.g)+abs(sc.b-nc.b);
 }
 
 vec4 get_pixel(in sampler2D tex, in vec2 coords, in float dx, in float dy) {
@@ -36,8 +36,8 @@ float IsEdge(in sampler2D tex, in vec2 coords){
   //float pix[9];
   //float pix[5];
   float cd[8];
-  int k = -1;
-  float delta;
+  //int k = -1;
+  //float delta;
 
   /*pix[0] = avg_intensity(get_pixel(tex,coords,float(0)*dxtex,float(0)*dytex));//color of itself
   pix[1] = avg_intensity(get_pixel(tex,coords,float(-1)*dxtex,float(-1)*dytex));
@@ -45,15 +45,22 @@ float IsEdge(in sampler2D tex, in vec2 coords){
   pix[3] = avg_intensity(get_pixel(tex,coords,float(-1)*dxtex,float(1)*dytex));
   pix[4] = avg_intensity(get_pixel(tex,coords,float(0)*dxtex,float(1)*dytex));*/
   vec4 sc = get_pixel(tex,coords,float(0)*dxtex,float(0)*dytex);
-  cd[0] = 0.0;//color_difference( sc, get_pixel(tex,coords,float(-1)*dxtex,float(-1)*dytex) );//color of itself
+  cd[0] = color_difference( sc, get_pixel(tex,coords,float(-1)*dxtex,float(-1)*dytex) );//color of itself
   cd[1] = color_difference( sc, get_pixel(tex,coords,float(-1)*dxtex,float(0)*dytex) );
   cd[2] = color_difference( sc, get_pixel(tex,coords,float(-1)*dxtex,float(1)*dytex) );
   cd[3] = color_difference( sc, get_pixel(tex,coords,float(0)*dxtex,float(1)*dytex) );
 
-  cd[4] = 0.0;//color_difference( sc, get_pixel(tex,coords,float(1)*dxtex,float(1)*dytex) );//check the other angle incase its over alpha, so we can add it too
-  cd[5] = 0.0;//color_difference( sc, get_pixel(tex,coords,float(1)*dxtex,float(0)*dytex) );
-  cd[6] = 0.0;//color_difference( sc, get_pixel(tex,coords,float(1)*dxtex,float(-1)*dytex) );
-  cd[7] = 0.0;//color_difference( sc, get_pixel(tex,coords,float(0)*dxtex,float(-1)*dytex) );
+  vec4 alt1 = get_pixel(tex,coords,float(1)*dxtex,float(1)*dytex);
+  vec4 alt2 = get_pixel(tex,coords,float(1)*dxtex,float(0)*dytex);
+  vec4 alt3 = get_pixel(tex,coords,float(1)*dxtex,float(-1)*dytex);
+  vec4 alt4 = get_pixel(tex,coords,float(0)*dxtex,float(-1)*dytex);
+
+  //cd[4] = color_difference( sc, alt1 );
+  if( length(alt1.rgb) < 0.1 ){ cd[4] = color_difference( sc, alt1 ); }else{ cd[4]=0.0; }
+  if( length(alt2.rgb) < 0.1 ){ cd[5] = color_difference( sc, alt2 ); }else{ cd[5]=0.0; }
+  if( length(alt3.rgb) < 0.1 ){ cd[6] = color_difference( sc, alt3 ); }else{ cd[6]=0.0; }
+  if( length(alt4.rgb) < 0.1 ){ cd[7] = color_difference( sc, alt4 ); }else{ cd[7]=0.0; }
+  //check the other angle incase its over alpha, so we can add it too
   // read neighboring pixel intensities
   /*for (int i=-1; i<2; i++) {
     for(int j=-1; j<2; j++) {
@@ -95,16 +102,16 @@ void PS()
 
 
     #ifdef BASE
-        vec4 diffColor = cMatDiffColor;
-        diffColor *= vColor;
+        //vec4 diffColor = cMatDiffColor;
+        vec4 diffColor = vColor;
         gl_FragColor = diffColor;
     #endif
 
     #ifdef EDGE
       vec4 color = vec4(0.0,0.0,0.0,1.0);
-      if(IsEdge(sEnvMap,vScreenPos.xy / vScreenPos.w)>0.1){
-        //color.rgba = vec4(1.0);
-        color = get_pixel(sEnvMap,vScreenPos.xy / vScreenPos.w,float(0)*(1.0/1920.0),float(0)*(1.0/1080.0));
+      if(IsEdge(sEnvMap,vScreenPos.xy / vScreenPos.w)>=2.0){
+        color.rgba = vec4(1.0);
+        //color = get_pixel(sEnvMap,vScreenPos.xy / vScreenPos.w,float(0)*(1.0/1920.0),float(0)*(1.0/1080.0));
         //color.rgba = diffColor;
         //color.g = IsEdge(sEnvMap,vScreenPos.xy / vScreenPos.w);
         //color.a = 1.0;
