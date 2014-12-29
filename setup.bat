693060d60ee8cc7e0006829607fc6dde2ef541fb
ghidra <ghidra@gmail.com>
2014-12-28 21:16:44 -0500

    working bat file

--- setup.bat
+++ setup.bat
@@ -1,27 +1,46 @@
 @echo OFF
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
+
+Echo -----------------------
 
 Echo Launch dir: "%~dp0"
 Echo Current dir: "%CD%"
-
 Echo Passed in dir: "%1"
 
-for /D %%f in (*.*) do (
-  set FOLDER=%~dp0%%f
-  if "%%f"=="Scripts" (
-    set NEWFOLDER = %1\Bin\Data\research
-    if exist NEWFOLDER (
-      echo Scripts Folder already exists
-    ) else (
-      echo we need to make link !NEWFOLDER!
-    )
-  ) else (
-    set NEWFOLDER = %1\Bin\Extra\%%f
-    if exist NEWFOLDER (
-      echo !FOLDER! already exist
-    ) else (
-      echo we need to make !NEWFOLDER!
-    )
+SET URHOPATH=%1
+
+if exist "%URHOPATH%" (
+  SET "URHOBINPATH=!URHOPATH!\Bin\"
+  SET "URHODATAPATH=!URHOPATH!\Bin\Data\"
+  SET "URHOCOREDATAPATH=!URHOPATH!\Bin\CoreData\"
+
+  echo valid path "!URHOPATH!"
+  echo bin: "!URHOBINPATH!"
+  echo data: "!URHODATAPATH!"
+  echo core: "!URHOCOREDATAPATH!"
+
+  for /D %%f in (*.*) do (
+    SET FOLDER=%~dp0%%f
+    ::http://stackoverflow.com/questions/17279114/split-path-and-take-last-folder-name-in-batch-script
+    set MYDIR=!FOLDER:~0!
+    for %%f in (!MYDIR!) do set myfolder=%%~nxf
+
+    if "!myfolder!"=="Scripts" call:makeAlias "!myfolder!" "!FOLDER!" "!URHODATAPATH!!myfolder!\research"
+    if "!myfolder!"=="RenderPaths" call:makeAlias "!myfolder!" "!FOLDER!" "!URHOCOREDATAPATH!!myfolder!\research"
+    if "!myfolder!"=="Techniques" call:makeAlias "!myfolder!" "!FOLDER!" "!URHOCOREDATAPATH!!myfolder!\research"
+    if "!myfolder!"=="Shaders" call:makeAlias "!myfolder!" "!FOLDER!" "!URHOCOREDATAPATH!!myfolder!\research"
+    if "!myfolder!"=="Materials" call:makeAlias "!myfolder!" "!FOLDER!" "!URHODATAPATH!!myfolder!\research"
+
   )
 
+) else (
+  echo invalid path given: "%URHOPATH%"
 )
+
+Echo -----------------------
+
+GOTO:EOF
+
+:makeAlias
+mklink /J %~3 %~2
+GOTO:EOF
