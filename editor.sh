0af0eaaf7bfa71901c5e7a68e6f9f492094457f7
ghidra <ghidra@gmail.com>
2015-12-22 23:47:27 -0500

    ignore

--- editor.sh
+++
@@ -1,1 +0,0 @@
-/home/jimmy/projects/urho/Urho_DYN/bin/Urho3DPlayer /Scripts/Editor.as -pp /home/jimmy/projects/urho/Urho_DYN/bin -p "CoreData;Data;Research" -w -s -renderpath RenderPaths/ForwardPixelQuad.xml
