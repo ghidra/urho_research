5b98738aa10c40155d48e484c950add6fb412a33
ghidra <ghidra@gmail.com>
2016-05-23 20:14:26 -0400

    same place

--- Shaders/GLSL/lbm_advect.glsl
+++ Shaders/GLSL/lbm_advect.glsl
@@ -4,7 +4,7 @@
 #include "Transform.glsl"
 #include "ScreenPos.glsl"
 
-#define VEL 0.1
+#define VEL 0.025
 //#define IMG
 
 varying vec2 vTexCoord;
@@ -19,38 +19,45 @@ uniform sampler2D sDetailMap2;
 void PS( )// out vec4 fragColor, in vec2 fragCoord
 {
 	vec2 res = 1.0/cGBufferInvSize;
-	vec2 pix = vScreenPos2*res;//the pixel coordinate
-
-	int ix = int(floor(pix.x/2.0));
-	int iy = int(floor(pix.y/2.0));
-	vec2 sam = texture2D(sDiffMap, (vec2(2*ix+1,2*iy+1)+0.5)/res).yz;
-	float solid = texture2D(sDiffMap, (vec2(2*ix+1,2*iy+1)+0.5)/res).x;
 	
-	//vec2 sam = texture2D(iChannel0, u ).yz;
-	
-	vec2 dir=(normalize(sam));
-	//dir -= vec2(0.5,0.5);
-	
-	vec3 col = texture2D( sDetailMap1, vScreenPos2-((dir*length(sam))*VEL*cDeltaTimePS) ).xyz;////this is it trying to do the ping pong.... if we need to, which we might
-	
-	if(solid>0.5)
-	{
-		//#ifdef IMG
-		//col=vec3(1.,1.,0.0);//texture2D(sDetailMap2, vScreenPos2 ).xyz;
-		//#else
-		//col=vec3(vScreenPos2,0.0);
-		//#endif
-		col=texture2D(sDetailMap1, vScreenPos2 ).xyz;
-	}
-	float cc = length(sam);
-
-	gl_FragColor = vec4(col,1.0);
-	//gl_FragColor = vec4(abs(sam),0.0,1.0);
-
+	int firstpass=0;
 	#ifdef FIRSTPASS
-	if( cElapsedTimePS<=0.0 ) //initialisation
-	{
-		gl_FragColor  = vec4(vScreenPos2,0.0,1.0);
-	}
+	firstpass=1;
 	#endif
+
+	if( cElapsedTimePS<=0.0 && firstpass>0 )
+	{
+		gl_FragColor  = vec4(vTexCoord,0.0,1.0);
+	}
+	else
+	{
+		int ix = int(floor(gl_FragCoord.x/2.0));
+		int iy = int(floor(gl_FragCoord.y/2.0));
+		vec2 sam = texture2D(sDiffMap, (vec2(2*ix+1,2*iy+1)+0.5)/res).yz;
+		float solid = texture2D(sDiffMap, (vec2(2*ix+1,2*iy+1)+0.5)/res).x;
+		
+		//vec2 sam = texture2D(iChannel0, u ).yz;
+		
+		vec2 dir=(normalize(sam));
+		//dir -= vec2(0.5,0.5);
+		
+		vec3 col = texture2D( sDetailMap1, vTexCoord-(dir*length(sam)*VEL*cDeltaTimePS) ).xyz;////this is it trying to do the ping pong.... if we need to, which we might
+		//vec3 col = vec3(vTexCoord-((dir*length(sam)*VEL*cDeltaTimePS)),0.0);////this is it trying to do the ping pong.... if we need to, which we might
+		//vec3 col = texture2D(sDetailMap2,vTexCoord-((dir*length(sam)))).xyz;////this is it trying to do the ping pong.... if we need to, which we might
+		if(solid>0.5)
+		{
+			//#ifdef IMG
+			//col=vec3(1.,1.,0.0);//texture2D(sDetailMap2, vScreenPos2 ).xyz;
+			//#else
+			//col=vec3(vScreenPos2,0.0);
+			//#endif
+			col=texture2D(sDetailMap1, vTexCoord ).xyz;
+			//col=vec3(vTexCoord,0.0);
+		}
+		//float cc = length(sam);
+		//gl_FragColor=vec4(vec3(cc),1.0);
+		gl_FragColor = vec4(col,1.0);
+	}
+	//gl_FragColor  = vec4(vTexCoord,0.0,1.0);
+	//gl_FragColor = vec4(abs(sam),0.0,1.0);
 }
--- Shaders/GLSL/lbm_render.glsl
+++ Shaders/GLSL/lbm_render.glsl
@@ -21,24 +21,41 @@ void VS()
 
 void PS()
 {
-	vec4 stow_color =  texture2D(sDiffMap,vScreenPos2);
+  vec2 res = 1.0/cGBufferInvSize;
+
+  //vec2 half = res-1.0;//make it so we land in the middle of a pixel
+  //vec2 pix = (vScreenPos2*half)+0.5;//fragcoord at half pixel...this seems to be crap
+  //vec2 hpix = pix*(1.0/half);//normalized
+
+	//vec4 stow_color =  texture2D(sDiffMap,hpix);
 	vec4 lbm_color =  texture2D(sDetailMap1,vTexCoord);
 
-	vec2 res = 1.0/cGBufferInvSize;
-	vec2 pix = vScreenPos2*res;//the pixel coordinate
-	vec2 pix2 = vTexCoord*res;//the pixel coordinate
 	//only one pixel out of 4 stores the moments
-    	int ix = int(floor(pix2.x/2.0));
-	int iy = int(floor(pix2.y/2.0));
+  int ix = int(floor(gl_FragCoord.x/2.0));
+	int iy = int(floor(gl_FragCoord.y/2.0));
 	vec3 m = texture2D(sDetailMap1, (vec2(2*ix+1,2*iy+1)+0.5)/res).xyz;
+  //vec3 m = texture2D(sDetailMap1, (vec2(2*ix-0,2*iy-0)+0.5)/res).xyz;//RED
+  //vec3 m = texture2D(sDetailMap1, (vec2(2*ix-1,2*iy-0)+0.5)/res).xyz;//GREEN
+  //vec3 m = texture2D(sDetailMap1, (vec2(2*ix-0,2*iy-1)+0.5)/res).xyz;//BLUE
+  //vec3 m = texture2D(sDetailMap1, vTexCoord).xyz;
 
 	//
-	vec2 center = res/2.0;
-	vec2 dir = normalize(pix2-center);
+	//vec2 center = res/2.0;
+	//vec2 dir = normalize(pix-center);
 
 	//gl_FragColor = vec4(lbm_color.xyz*2.0,lbm_color.a);
-	gl_FragColor = vec4(lbm_color);
-	//gl_FragColor = vec4(m*1.0,1.0);
+	//gl_FragColor = vec4(lbm_color);
+	gl_FragColor = vec4(m*10.0,1.0);
+  //gl_FragColor = vec4(res-vec2(1279.5,799.5),0.0,1.0);
 	//gl_FragColor = vec4(0.0,dir,1.0);
 	//gl_FragColor = vec4(stow_color,1.0);
+  //gl_FragColor = vec4(pix*(1.0/half),0.0,1.0);
+  //if( mod(int(floor(pix.x)), 2) == 0)
+  //{
+  //  gl_FragColor = vec4(1.0);
+  //}
+  //else
+  //{
+  //  gl_FragColor = vec4(vec3(0.0),1.0);
+  //}
 }
